a9921dd5a15c29f944dc625aee74e9c71b973b8e
mrdudov <mrdudov@gmail.com>
2023-03-08 17:07:51 +0300

    add postgresql

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
 
-app = FastAPI(title="FastAPI, Docker, and Traefik")
+from app.db import database, User
+
+
+app = FastAPI(title="FastAPI, Docker")
 
 
 @app.get("/")
-def read_root():
-    return {"hello": "world"}
+async def read_root():
+    return await User.objects.all()
+
+
+@app.on_event("startup")
+async def startup():
+    if not database.is_connected:
+        await database.connect()
+    # create a dummy entry
+    await User.objects.get_or_create(email="test@test.com")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()

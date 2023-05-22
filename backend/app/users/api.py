2d75f4071422feba6ad9f9ef6500b6fd5616fbf1
mrdudov <mrdudov@gmail.com>
2023-05-22 11:13:34 +0300

    add profile-img upload

--- backend/app/users/api.py
+++ backend/app/users/api.py
@@ -1,9 +1,10 @@
-from typing import List, Annotated
+from typing import List
 
-from fastapi import Depends, Depends, APIRouter, File, UploadFile
+from fastapi import Depends, Depends, APIRouter, UploadFile
 from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from fastapi_jwt_auth import AuthJWT
+import aiofiles
 
 from app.db import get_session
 from app.models import User, ReturnUser
@@ -25,11 +26,15 @@ async def get_users(
     return [ReturnUser(id=user.id, email=user.email) for user in users]
 
 
-@router.post("/files/")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
+@router.post("/profile-img/", dependencies=[Depends(FastapiJwtAuthBearer())])
+async def create_upload_file(file: UploadFile, Authorize: AuthJWT = Depends()):
 
-
-@router.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
+    # TODO: fix file name and file path
+    current_user = Authorize.get_jwt_subject()
+    file_name = f"/usr/src/data_base/user_profile_img/{current_user}.{file.filename}"
+        
+    async with aiofiles.open(file_name, 'wb+') as out_file:
+        while content := await file.read(1024):
+            await out_file.write(content)
+    print(current_user)
     return {"filename": file.filename}

52e86a05cf939fe305607886c211b33eb10d8d1c
mrdudov <mrdudov@gmail.com>
2023-05-26 17:20:13 +0300

    websocket get online users WIP

--- backend/app/websocket/connect_manager.py
+++ backend/app/websocket/connect_manager.py
@@ -2,22 +2,20 @@ from fastapi import WebSocket
 from app.settings import SETTINGS
 from app.websocket.exceptions import OnlineLimitException
 
+
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.active_connections: list[tuple[WebSocket, str]] = []
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, websocket: WebSocket, token: str):
         if len(self.active_connections) >= SETTINGS.max_online_users:
             raise OnlineLimitException
         await websocket.accept()
-        self.active_connections.append(websocket)
+        self.active_connections.append((websocket, token))
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+    def disconnect(self, websocket: WebSocket, token: str):
+        self.active_connections.remove((websocket, token))
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
+    async def send_online_users_list(self, websocket: WebSocket):
+        online_users_list = str([item[1] for item in self.active_connections])
+        await websocket.send_text(online_users_list)
--- backend/app/websocket/websocket.py
+++ backend/app/websocket/websocket.py
@@ -11,6 +11,7 @@ from fastapi_jwt_auth import AuthJWT
 
 from app.websocket.connect_manager import ConnectionManager
 from app.websocket.functions import get_token
+from app.websocket.exceptions import NoTokenException
 
 
 manager = ConnectionManager()
@@ -18,47 +19,14 @@ router = APIRouter(prefix="/ws", tags=["ws"])
 
 
 html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <label>Item ID: <input type="text" id="itemId" autocomplete="off" value="foo"/></label>
-            <label>Token: <input type="text" id="token" autocomplete="off" value="some-key-token"/></label>
-            <button onclick="connect(event)">Connect</button>
-            <hr>
-            <label>Message: <input type="text" id="messageText" autocomplete="off"/></label>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-        var ws = null;
-            function connect(event) {
-                var itemId = document.getElementById("itemId")
-                var token = document.getElementById("token")
-                ws = new WebSocket("ws://localhost:8080/ws/items/" + itemId.value + "/ws?token=" + token.value);
-                ws.onmessage = function(event) {
-                    var messages = document.getElementById('messages')
-                    var message = document.createElement('li')
-                    var content = document.createTextNode(event.data)
-                    message.appendChild(content)
-                    messages.appendChild(message)
-                };
-                event.preventDefault()
-            }
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
+    var url = "ws://localhost:8080/ws"
+    var token = "some_token"
+    ws = new WebSocket(`${url}/online_users?token=${token}`)
+    ws.onmessage = function(event) {
+        console.log(event.data)
+    }
+
+    ws.send('get_online_users')
 """
 
 
@@ -67,33 +35,21 @@ async def get():
     return HTMLResponse(html)
 
 
-@router.websocket("/items/{item_id}/ws")
-async def websocket_endpoint(
-    *,
-    websocket: WebSocket,
-    item_id: str,
-    token: Annotated[str, Depends(get_token)],
-):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"token value is: {token}")
-        await websocket.send_text(f"Message text was: {data}, for item ID: {item_id}")
-
-
 @router.websocket("/online_users")
 async def websocket_endpoint(
     *,
     websocket: WebSocket,
     token: Annotated[str, Depends(get_token)],
 ):
+    if token == "":
+        raise NoTokenException
+
     print(f"{token=}")
-    await manager.connect(websocket)
+    await manager.connect(websocket, token)
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client # says: {data}")
+            if data == "get_online_users":
+                await manager.send_online_users_list(websocket)
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client # left the chat")
+        manager.disconnect(websocket, token)
